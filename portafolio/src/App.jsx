a5d4625a05923f9b1295a86a0f0f53f5eb4d9a5e
ulldecorb <ausias.bertran@gmail.com>
2022-07-18 19:01:58 +0200

    Add basic style on App component Set typos on sass variables

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Artist } from './constants/projects';
 import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>{Artist.artistName}</h2>
-        <h2>Art & Design portofolio</h2>
+      <header className="App__header">
+        <img src={logo} className="App__logo" alt="logo" />
+        <footer className="App__footer">
+          <h1 className="footer__title">{Artist.artistName}</h1>
+          <h2 className="footer__title">Art & Design portofolio</h2>
+        </footer>
       </header>
     </div>
   );
