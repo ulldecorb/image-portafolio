294f57e81dc8db34fb3d5da752311882c5a094e9
ulldecorb <ausias.bertran@gmail.com>
2022-07-19 01:51:53 +0200

    Add button element on skill-box class

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -5,41 +5,34 @@ import './style/App.css';
 
 function App() {
   const [state, setState] = React.useState([]);
-  // const [galleryList, setGalleryList] = React.useState([]);
-
-  // const getGallery = () => {
-  //   const newList = State.map((skill) => {
-  //     const innerNewList = {
-  //       title: skill.skillName,
-  //       thumbnailUrl: [
-  //         skill.skillCollection[0].galleryCollection[0].imageUrl || null,
-  //         skill.skillCollection[1].galleryCollection[0].imageUrl || null
-  //       ]
-  //     };
-  //     console.log('innerNewList', innerNewList);
-  //     return innerNewList;
-  //   });
-
-  //   setGalleryList(newList);
-  // };
+  const [mainGallery, setMainGallery] = React.useState([]);
 
   React.useEffect(() => {
     setState(State);
   }, []);
 
+  const handleHome = (e, skillTitle) => {
+    e.preventDefault();
+    setMainGallery(skillTitle);
+  };
+
+  console.log('mainGallery: ', mainGallery);
+
   return (
     <div className="App">
       <header className="App__header">
         <img src={logo} className="App__logo" alt="logo" />
-        {state.map((skill) => (
-          <section className="App__skill-box" key={`link-${skill.skillName}`}>
-            <h3 className="skill-box__title">{skill.skillName}</h3>
-            <div className="skill-box__thumbnail-box">
-              <img src={skill.skillCollection[0].galleryCollection[0].imageUrl} className="skill-box__thumbnail" alt="logo" />
-              <img src={skill.skillCollection[1].galleryCollection[0].imageUrl} className="skill-box__thumbnail" alt="logo" />
-            </div>
-          </section>
-        ))}
+        <section className="App__skill-box">
+          {state.map((skill) => (
+            <button type="button" onClick={(e) => handleHome(e, skill.skillName)} className="skill-box" key={`link-${skill.skillName}`}>
+              <h3 className="skill-box__title">{skill.skillName}</h3>
+              <div className="skill-box__thumbnail-box">
+                <img src={skill.skillCollection[0].galleryCollection[0].imageUrl} className="skill-box__thumbnail" alt="logo" />
+                <img src={skill.skillCollection[1].galleryCollection[0].imageUrl} className="skill-box__thumbnail" alt="logo" />
+              </div>
+            </button>
+          ))}
+        </section>
         <footer className="App__footer">
           <h1 className="footer__title">{Artist.artistName}</h1>
           <h2 className="footer__title">Art & Design portofolio</h2>
