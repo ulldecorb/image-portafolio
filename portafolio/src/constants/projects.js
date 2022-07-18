21416191dd2b299fd2ae1faa2155ac731533ce77
ulldecorb <ausias.bertran@gmail.com>
2022-07-18 17:55:32 +0200

    Create basic state constants to develop

---
+++ portafolio/src/constants/projects.js
@@ -0,0 +1,92 @@
+export const Artist = {
+  artistname: 'Marti Rosell',
+  contact: {
+    adress: 'Carrer Viladedalt, 13, Viladebaix, 08666',
+    mail: 'martirosellart&design@gmail.com'
+  }
+};
+
+export const State = [
+  {
+    categoryName: 'galaxy',
+    galleryCollection: [
+      {
+        projectName: 'hubble',
+        discipline: 'Fotolibro y exposició',
+        sinopsi: 'Lorem ipsum....',
+        data: '2022',
+        description: 'Lorem Ipsum...',
+        imagesStock: [
+          {
+            imageName: 'Start the observer',
+            imageUrl: 'https://helios-i.mashable.com/imagery/articles/008mlMlr0cEjIUKunxTdVYo/hero-image.fill.size_1248x702.v1623390847.jpg',
+            hashtag: ['#noir', '#photobook', '#exhibition']
+          },
+          {
+            imageName: 'super nova collection',
+            imageUrl: 'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2022/05/a_dazzling_hubble_collection_of_supernova_host_galaxies/24065747-1-eng-GB/A_dazzling_Hubble_collection_of_supernova_host_galaxies.jpg',
+            hashtag: ['#laterracalenta', '#photobook', '#exhibition']
+          },
+          {
+            imageName: 'Random galaxy',
+            imageUrl: 'https://www.nasa.gov/sites/default/files/thumbnails/image/potw2120a.jpg',
+            hashtag: ['#noir', '#photobook', '#exhibition']
+          }
+        ]
+      },
+      {
+        projectName: 'webb',
+        discipline: 'Fotolibro y exposició',
+        sinopsi: 'Lorem ipsum....',
+        data: '2022',
+        description: 'Lorem Ipsum...',
+        imagesStock: [
+          {
+            imageName: 'Webb around the sky',
+            imageUrl: 'http://i2.cdn.turner.com/cnnnext/dam/assets/220712092620-04-james-webb-telescope-first-images-0712-carina-nebula.jpg',
+            hashtag: ['#noir', '#photobook', '#exhibition']
+          },
+          {
+            imageName: 'The eye',
+            imageUrl: 'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2022/05/a_dazzling_hubble_collection_of_supernova_host_galaxies/24065747-1-eng-GB/A_dazzling_Hubble_collection_of_supernova_host_galaxies.jpghttps://images.theconversation.com/files/474234/original/file-20220715-18-x7oyaq.png?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop',
+            hashtag: ['#laterracalenta', '#photobook', '#exhibition']
+          },
+          {
+            imageName: 'Glitter',
+            imageUrl: 'https://www.nasa.gov/sites/default/files/thumbnails/image/stsci-j-p2207c-f.jpg',
+            hashtag: ['#noir', '#photobook', '#exhibition']
+          }
+        ]
+      }
+    ]
+  },
+  {
+    categoryName: 'sky',
+    galleryCollection: [
+      {
+        projectName: 'hubble',
+        discipline: 'Fotolibro y exposició',
+        sinopsi: 'Lorem ipsum....',
+        data: '2022',
+        description: 'Lorem Ipsum...',
+        imagesStock: [
+          {
+            imageName: 'Start the observer',
+            imageUrl: 'https://helios-i.mashable.com/imagery/articles/008mlMlr0cEjIUKunxTdVYo/hero-image.fill.size_1248x702.v1623390847.jpg',
+            hashtag: ['#noir', '#photobook', '#exhibition']
+          },
+          {
+            imageName: 'super nova collection',
+            imageUrl: 'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2022/05/a_dazzling_hubble_collection_of_supernova_host_galaxies/24065747-1-eng-GB/A_dazzling_Hubble_collection_of_supernova_host_galaxies.jpg',
+            hashtag: ['#laterracalenta', '#photobook', '#exhibition']
+          },
+          {
+            imageName: 'Random galaxy',
+            imageUrl: 'https://www.nasa.gov/sites/default/files/thumbnails/image/potw2120a.jpg',
+            hashtag: ['#noir', '#photobook', '#exhibition']
+          }
+        ]
+      }
+    ]
+  }
+];
