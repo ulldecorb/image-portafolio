da4192925c3ae8f30c01d7ce1b0c1307d13c5fe5
ulldecorb <ausias.bertran@gmail.com>
2022-09-30 12:04:20 +0200

    Remove navbar section on detail component

--- portafolio/src/components/Detail/index.jsx
+++ portafolio/src/components/Detail/index.jsx
@@ -13,6 +13,7 @@ export function Detail({ state, artist }) {
 
   const [coverNavbarFocus, setCoverScrollFocus] = useState(true);
   const [galleryNavbarFocus, setGalleryScrollFocus] = useState(false);
+  console.log(galleryNavbarFocus);
   const [aboutMeNavbarFocus, setAboutMeScrollFocus] = useState(false);
 
   const getDetail = () => {
@@ -73,36 +74,17 @@ export function Detail({ state, artist }) {
         id="detail"
         onScroll={handleScroll}
       >
-        <nav className="detail__navbar">
-          <a
-            style={{ textDecoration: 'none' }}
-            href={`https://ulldecorb.github.io/image-portafolio/#/martirosell/${galleryParam}/${detailParam}/#cover`}
-            className="navbar-box"
-          >
-            <p className="navbar-box__title-section">Cover</p>
-            <span className={coverNavbarFocus ? 'navbar-box__selected-box navbar-box__selected-box--focused' : 'navbar-box__selected-box'} />
-          </a>
-          <a
-            style={{ textDecoration: 'none' }}
-            href={`https://ulldecorb.github.io/image-portafolio/#/martirosell/${galleryParam}/${detailParam}/#gallery`}
-            className="navbar-box"
-          >
-            <p className="navbar-box__title-section">Gallery</p>
-            <span className={galleryNavbarFocus ? 'navbar-box__selected-box navbar-box__selected-box--focused' : 'navbar-box__selected-box'} />
-          </a>
-          <a
-            style={{ textDecoration: 'none' }}
-            href={`/martirosell/${galleryParam}/${detailParam}/#aboutMe`}
-            className="navbar-box"
-          >
-            <p className="navbar-box__title-section">About me</p>
-            <span className={aboutMeNavbarFocus ? 'navbar-box__selected-box navbar-box__selected-box--focused' : 'navbar-box__selected-box'} />
-          </a>
-        </nav>
 
         <section className="detail__header">
           <h3 className="header__discipline">{aboutMeNavbarFocus ? '' : detail.discipline}</h3>
-          <h2 className={coverNavbarFocus ? 'header__title' : 'header__title header__title--reduced'}>{aboutMeNavbarFocus ? 'ABOUT ME' : detail.detailName.toUpperCase()}</h2>
+          <h2 className={coverNavbarFocus
+            ? 'header__title'
+            : 'header__title header__title--reduced'}
+          >
+            {aboutMeNavbarFocus
+              ? 'ABOUT ME'
+              : detail.detailName.toUpperCase()}
+          </h2>
         </section>
 
         <section id="viewer" className={activeViewer ? 'detail__viewer' : 'detail__viewer--hidden'}>
