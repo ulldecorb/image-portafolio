3e3cf8b52a362d15353c258d5f6ea4e0f06eccaf
ulldecorb <ausias.bertran@gmail.com>
2022-11-19 01:51:29 +0100

    Add conditional dark background style on footer

--- portafolio/src/components/Footer/index.jsx
+++ portafolio/src/components/Footer/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './footer.css';
 
 export function Footer({ artist }) {
+  const currentUrl = useLocation();
+  const [currentGalleryName, setCurrentGalleryName] = useState('');
+
+  const checkUri = () => (currentUrl.pathname === '/');
+  const getUriSGalleryName = () => {
+    if (checkUri()) return null;
+    const galleryNameRegExp = /([a-z]+)/i;
+    const newGalleryName = galleryNameRegExp.exec(currentUrl.pathname)[0];
+    return newGalleryName;
+  };
+
+  useEffect(() => {
+    setCurrentGalleryName(getUriSGalleryName());
+  }, [currentUrl]);
+
   return (
-    <footer className="footer">
+    <footer className={currentGalleryName === 'about' ? 'footer footer--dark-theme' : 'footer'}>
       <Link
         to="/about"
         style={{ textDecoration: 'none' }}
